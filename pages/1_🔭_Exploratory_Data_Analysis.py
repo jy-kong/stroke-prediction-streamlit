fb8fb9e21b06792078be6a5b418e5bca28a6e210
jy-kong <73959291+jy-kong@users.noreply.github.com>
2025-01-17 17:05:19 +0800

    Update 1_🔭_Exploratory_Data_Analysis.py

--- pages/1_🔭_Exploratory_Data_Analysis.py
+++ pages/1_🔭_Exploratory_Data_Analysis.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from pandas_profiling import ProfileReport
+import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(layout="wide", page_title='Stroke Prediction Oracle', page_icon='🔮')
