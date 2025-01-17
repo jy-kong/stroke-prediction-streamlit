aed85f2068fb5f18e4e4a20932864a53cf5bb6fa
jy-kong <73959291+jy-kong@users.noreply.github.com>
2025-01-17 16:54:14 +0800

    Update 1_🔭_Exploratory_Data_Analysis.py

--- pages/1_🔭_Exploratory_Data_Analysis.py
+++ pages/1_🔭_Exploratory_Data_Analysis.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-from pandas_profiling import ProfileReport  # Explicitly import ProfileReport to avoid issues
-from pydantic_settings import BaseSettings
+from ydata_proifiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(layout="wide", page_title='Stroke Prediction Oracle', page_icon='🔮')
