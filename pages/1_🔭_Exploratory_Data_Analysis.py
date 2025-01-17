1801ef2276d2df219a2da59e2a07314d4d40a0ee
jy-kong <73959291+jy-kong@users.noreply.github.com>
2025-01-17 16:44:51 +0800

    Update 1_🔭_Exploratory_Data_Analysis.py

--- pages/1_🔭_Exploratory_Data_Analysis.py
+++ pages/1_🔭_Exploratory_Data_Analysis.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import pandas_profiling
+from pandas_profiling import ProfileReport  # Explicitly import ProfileReport to avoid issues
 from pydantic_settings import BaseSettings
 from streamlit_pandas_profiling import st_profile_report
 
